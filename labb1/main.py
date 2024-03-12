5e179d2bbb35b557d97a8f5dafeb5b18070f4223
KevinKinnunen <54663592+KevinKinnunen@users.noreply.github.com>
2024-03-12 23:57:12 +0100

    Create main.py

---
+++ labb1/main.py
@@ -0,0 +1,15 @@
+## Grundläggande algoritmen:
+# 1. Läs in datan och spara i lämplig datastruktur (pichu.txt, pikachu.txt)
+# 2. Plotta alla punkterna med olika färger i samma fönster
+# 3. Läs in testpunkter (test_points.txt)
+# 4. Beräkna avstånd mellan testpunkt och övriga punkter
+# 5. Närmaste punkten tillhör Pichu?
+#    ...
+# 6. JA = Klassificera testpunkt som Pichu
+#    NEJ = Klassificera testpunkt som Pikachu
+
+## The answer key for the given test data:
+# Sample with (width, height): (25, 35) classified as Pikachu
+# Sample with (width, height): (15, 14) classified as Pichu
+# Sample with (width, height): (26, 30) classified as Pichu
+# Sample with (width, height): (22, 45) classified as Pikachu
